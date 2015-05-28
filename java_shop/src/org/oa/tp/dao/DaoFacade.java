1e16c51394091b4d2708bf809731d1fd3b135a8e
Mykola Dymura <nikolay.dymura@gmail.com>
2015-05-28 13:57:41 +0300

    added SQLite jdbc

--- java_shop/src/org/oa/tp/dao/DaoFacade.java
+++ java_shop/src/org/oa/tp/dao/DaoFacade.java
@@ -3,7 +3,22 @@ package org.oa.tp.dao;
 import org.oa.tp.data.Album;
 import org.oa.tp.data.Genre;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+
 public class DaoFacade {
+
+	public DaoFacade() {
+		Connection c = null;
+		try {
+			Class.forName("org.sqlite.JDBC");
+			c = DriverManager.getConnection("jdbc:sqlite:test.db");
+		} catch ( Exception e ) {
+			System.err.println( e.getClass().getName() + ": " + e.getMessage() );
+			System.exit(0);
+		}
+		System.out.println("Opened database successfully");
+	}
 
 	private final AlbumDao albumDao = new AlbumDao();
 	
