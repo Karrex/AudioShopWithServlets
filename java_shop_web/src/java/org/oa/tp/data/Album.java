c714555dee4f8ba4c27f429190c2c0988c7cbdc4
Mykola Dymura <nikolay.dymura@gmail.com>
2015-06-04 12:54:38 +0300

    added servlets api as rest part

--- java_shop_web/src/java/org/oa/tp/data/Album.java
+++ java_shop_web/src/java/org/oa/tp/data/Album.java
@@ -4,50 +4,66 @@ import com.google.gson.annotations.SerializedName;
 
 public class Album {
 
-	@SerializedName("id")
-	private final long id;
-	
-	@SerializedName("name")
-	private String name;
-	
-	@SerializedName("year")
-	private int year;
-	
-	public Album(long id, String name, int year) {
-		
-		this.id = id;
-		this.name = name;
-		this.year = year;
-	}
-	public String getName() {
-		return name;
-	}
-	public void setName(String name) {
-		this.name = name;
-	}
-	public int getYear() {
-		return year;
-	}
-	public void setYear(int year) {
-		this.year = year;
-	}
-	public long getId() {
-		return id;
-	}
+    @SerializedName("id")
+    private final long id;
 
-	@Override
-	public boolean equals(Object o) {
-		if (this == o) return true;
-		if (o == null || getClass() != o.getClass()) return false;
+    @SerializedName("name")
+    private String name;
 
-		Album album = (Album) o;
+    @SerializedName("year")
+    private int year;
 
-		return id == album.id;
+    public Album(long id, String name, int year) {
 
-	}
+        this.id = id;
+        this.name = name;
+        this.year = year;
+    }
+    
+    public Album(String name, int year) {
 
-	@Override
-	public int hashCode() {
-		return (int) (id ^ (id >>> 32));
-	}
+        this.id = 0;
+        this.name = name;
+        this.year = year;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getYear() {
+        return year;
+    }
+
+    public void setYear(int year) {
+        this.year = year;
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) {
+            return true;
+        }
+        if (o == null || getClass() != o.getClass()) {
+            return false;
+        }
+
+        Album album = (Album) o;
+
+        return id == album.id;
+
+    }
+
+    @Override
+    public int hashCode() {
+        return (int) (id ^ (id >>> 32));
+    }
 }
--- java_shop_web/src/java/org/oa/tp/servlets/AlbumsViewServlet.java
+++ java_shop_web/src/java/org/oa/tp/servlets/AlbumsViewServlet.java
@@ -19,27 +19,9 @@ import org.oa.tp.data.Album;
  *
  * @author nikolay
  */
-public class AlbumsServlet extends HttpServlet {
+public class AlbumsViewServlet extends HttpServlet {
 
     protected void processRequest(HttpServletRequest request, HttpServletResponse response)
-            throws ServletException, IOException {
-        response.setContentType("text/html;charset=UTF-8");
-        try (PrintWriter out = response.getWriter()) {
-            /* TODO output your page here. You may use following sample code. */
-            out.println("<!DOCTYPE html>");
-            out.println("<html>");
-            out.println("<head>");
-            out.println("<title>Servlet AlbumsServlet</title>");
-            out.println("</head>");
-            out.println("<body>");
-            out.println("<h1>Servlet AlbumsServlet at " + request.getContextPath() + "</h1>");
-            out.println("</body>");
-            out.println("</html>");
-        }
-    }
-
-    @Override
-    protected void doGet(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
         DaoFacade facade = new DaoFacade(getServletContext());
         List<Album> albums = facade.getAlbumDao().loadAll();
@@ -58,29 +40,60 @@ public class AlbumsServlet extends HttpServlet {
             out.println("<th>ID</th>");
             out.println("<th>NAME</th>");
             out.println("<th>YEAR</th>");
+            out.println("<th>DELETE</th>");
             out.println("</tr>");
             for (Album album : albums) {
                 out.println("<tr>");
                 out.println("<td>" + album.getId() + "</td>");
-                out.println("<td>" +  album.getName() +"</td>");
-                out.println("<td>" + album.getYear()+"</td>");
+                out.println("<td>" + album.getName() + "</td>");
+                out.println("<td>" + album.getYear() + "</td>");
+                out.println("<td><a href=\"albums?method=delete&id=" + album.getId() + "\">Delete</a></td>");
                 out.println("</tr>");
             }
             out.println("</table>");
+            out.println("<a href=\"create_album.html\">Create</a>");
             out.println("</body>");
             out.println("</html>");
         }
     }
 
+    // <editor-fold defaultstate="collapsed" desc="HttpServlet methods. Click on the + sign on the left to edit the code.">
+    /**
+     * Handles the HTTP <code>GET</code> method.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        processRequest(request, response);
+    }
+
+    /**
+     * Handles the HTTP <code>POST</code> method.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
     @Override
     protected void doPost(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
         processRequest(request, response);
     }
 
+    /**
+     * Returns a short description of the servlet.
+     *
+     * @return a String containing servlet description
+     */
     @Override
     public String getServletInfo() {
         return "Short description";
-    }
+    }// </editor-fold>
 
 }
